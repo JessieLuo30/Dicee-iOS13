309a82dde4f62766b5ab3006366402ec762ffb11
Jessie Luo <jluo30@jhu.edu>
2022-07-09 00:13:39 -0700

    feat: roll random dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageViewOne.image = UIImage(named: "DiceSix")
+        diceImageViewTwo.image = UIImage(named: "DiceTwo")
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+        diceImageViewOne.image = diceArray.randomElement() as! UIImage
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
     }
 
 
 }
 
+bh
